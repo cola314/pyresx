163cb8c881ad3d9c297ad6db595f2eb9b9873535
yoobyungjo <ubj0314@naver.com>
2021-03-07 17:16:41 +0900

    Add files to upload package to pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyresx',
+    version='0.0.1',
+    description='.resx file writer for python',
+    url='https://github.com/cola314/pyresx.git',
+    author='cola314',
+    author_email='ubj020314@gmail.com',
+    license='MIT',
+    packages=['pyresx'],
+    install_requires=[]
+)
